59b843f6b9aba277803e2a6e1cc262caa7861ae8
Martin Banks <anotherbanksy@gmail.com>
2018-10-09 14:12:15 +1100

    sets up addpage post rout

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const next = require('next')
+const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next ({ dev })
 const handle = app.getRequestHandler()
 const port = 3000
 
+
 app.prepare()
   .then(() => {
     const server = express()
+
+    server.use(
+      bodyParser.json({ limit: '10mb', extended: true })
+    )
+    server.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
     server.get('/test', (req ,res) => {
       res.send('test route working')
     })
 
+    
+    server.post('/addpage', (req, res, next) => {
+      res.send('id recieved')
+      const { body } = req
+      console.log({ body })
+    })
+    
     server.get('*', (req, res) => {
       return handle(req, res)
     })
