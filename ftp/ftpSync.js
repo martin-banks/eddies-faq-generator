06ef7926f0f2f8adfdf642f8cfe5eda3794a12dd
Martin Banks <anotherbanksy@gmail.com>
2018-10-18 16:24:43 +1100

    adds new module for synchronus ftp

---
+++ ftp/ftpSync.js
@@ -0,0 +1,35 @@
+const FTP = require('ftp')
+// * https://www.npmjs.com/package/ftp
+
+exports.ftpSync = (c) => ({
+  list ({ path }) {
+    return new Promise((resolve, reject) => {
+      c.list(path, (err, list) => {
+        if (err) reject(err)
+        resolve(list)
+      })
+    })
+  },
+  put ({ file, from, into }) {
+    return new Promise((resolve, reject) => {
+      c.put(`${from}/${file}`, `${into}/${file}`, err => {
+        if (err) reject(err)
+        console.log(`-- SUCCESS --\n${into}/${file} was successfully uploaded`)
+        resolve(`-- SUCCESS --\n${into}/${file} was successfully uploaded`)
+      })
+    })
+  },
+  mkdir ({ into, name }) {
+    return new Promise((resolve, reject) => {
+      c.mkdir(`${into}/${name}`, err => {
+        if (err) reject(err)
+        console.log(`-- SUCCESS --\n${into}/${name} was successfully created`)
+        resolve(`-- SUCCESS --\n${into}/${name} was successfully created`)
+      })
+    })
+  },
+  end () {
+    c.end()
+  }
+})
+
